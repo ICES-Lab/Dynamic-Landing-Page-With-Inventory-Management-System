edf095e52afb1e6f84de9f5b4d64bbaff9f7e617
JaisuryaIT <jaisuryaitofficial@gmail.com>
2024-02-26 12:21:35 +0530

    Admin Panel Initialization using Filament Laraval

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
+// class User extends Authenticatable implements FilamentUser
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,4 +46,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    // public function canAccessPanel(Panel $panel): bool
+    // {
+    //     return str_ends_with($this->email, '@ices.com') && $this->hasVerifiedEmail();
+    // }
 }
